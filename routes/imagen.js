a27e429cc9c2893e3fb163b3ede954bd61d60f33
Miguel Gastalver Trujillo <mgastalver@gmail.com>
2016-12-13 23:08:23 +0100

    OK

--- routes/imagen.js
+++ routes/imagen.js
@@ -3,22 +3,44 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 var multer = require('multer');
-var upload = multer({dest: 'uploads/'});
-
-/* POST imagen */
-router.post('/',upload.single('archivo'),function(req,res,next){
-    /* TODO Localizar archivo en directorio temporal. */
-        var archivoSubido = req.file;
-        console.log(archivoSubido);
-        res.send("Subido");
-    /* TODO Comprobar que es un tipo de archivo admitido o rechazar. */
-    /* TODO Cambiar nombre */
-    /* TODO Copiar a nueva localización definitiva*/
-
-
-
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
 });
 
+var upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+            req.invalidUpload = 'Archivo no admitido';
+            return cb(null, false);
+        }
+        cb(null, true);
+    }}).single('archivo');
+
+
+/* POST imagen */
+router.post('/', function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            // An error occurred when uploading
+            console.log('Multer error:' + err);
+            return
+        }
+        // Everything went fine
+        if (!req.invalidUpload){
+            console.log('Uploaded');
+            res.send('Uploaded');
+        } else {
+            console.log(req.invalidUpload);
+            res.send(req.invalidUpload);
+        }
+    })
+})
 
 /* GET listado de imagenes. */
 router.get('/', function(req, res, next) {
